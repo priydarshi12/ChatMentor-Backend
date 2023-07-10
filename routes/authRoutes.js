9f24291c69d8d75f2bbcca3cb0e4003ecab3a143
priydarshi12 <21001016054.jcboseust.ac.in>
2023-07-10 14:00:05 +0530

    final commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,20 @@
-const express=require("express")
-const {registerController, loginController, logoutController}=require('../controllers/authController')
+const express = require("express");
+const {
+  registerController,
+  loginController,
+  logoutController,
+} = require("../controllers/authController");
 
 //router object
 
-const router=express.Router()
-
+const router = express.Router();
 
 //routes
 
 //register
-router.post('/register',registerController)
+router.post("/register", registerController);
 
 //login
-router.post('/login',loginController)
+router.post("/login", loginController);
 
-
-
-module.exports=router
-
+module.exports = router;
