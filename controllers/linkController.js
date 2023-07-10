f3b699f1a127345a2b0a338ff51e90ab682424d0
priydarshi12 <21001016054.jcboseust.ac.in>
2023-07-10 20:10:41 +0530

    updating frontend url

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -19,7 +19,6 @@ const transporter = nodemailer.createTransport({
 });
 module.exports.linkController = async (req, res, next) => {
   try {
-    
     const User = await user.findOne({ email: req.body.email });
     if (User.email) {
       const token = jwt.sign({ _id: User._id }, `${keysecret}`, {
@@ -38,7 +37,7 @@ module.exports.linkController = async (req, res, next) => {
           to: User.email,
           subject: "Sending Email For Password Reset.",
           // *****MUST UPDATE ***************
-          text: `This Link is valid for 5 Minutes http://localhost:3000/new-password/${User._id}/${setusertoken.verifytoken}`,
+          text: `This Link is valid for 5 Minutes https://chat-mentor69.vercel.app//new-password/${User._id}/${setusertoken.verifytoken}`,
         };
 
         try {
@@ -48,8 +47,7 @@ module.exports.linkController = async (req, res, next) => {
             } else {
             }
           });
-        } catch (err) {
-        }
+        } catch (err) {}
       }
 
       res.send({ msg: "Email has been sent", status: true });
